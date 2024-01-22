d66929da8833b5264e9b643089d5a20874fb653f
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-22 13:02:03 +0800

    Initial commit

---
+++ src/main/java/ru/otus/java/basic/homework/Employee.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homework;
+
+public class Employee {
+    String name;
+    Integer age;
+
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homework/javaHomework.java
@@ -0,0 +1,10 @@
+package ru.otus.java.basic.homework;
+
+import java.util.ArrayList;
+
+public class javaHomework {
+    public static void main(String[] args) {
+
+
+
+}
